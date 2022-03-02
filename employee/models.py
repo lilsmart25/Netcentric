f4f1af208c2870c44e0d6515ebc8c6c2a595a996
lilsmart25 <smithgodwin678@gmail.com>
2022-03-02 06:59:04 -0800

    Final Commit

---
+++ employee/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    sno=models.CharField(max_length=20)
+    todo=models.CharField(max_length=100)
+    date=models.CharField(max_length=100)
+    description=models.CharField(max_length=1000)
+    
+    
+def __str__(self):
+    return "%s"  %(self.todo)
+
+class Meta:
+    db_table = "todo"
+    
