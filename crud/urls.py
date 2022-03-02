a3ce1dd9bc32b0b05401cd825852ec4c4320509f
Dxnzel123 <100784643+Dxnzel123@users.noreply.github.com>
2022-03-02 16:53:37 +0100

    Update urls.py

--- crud/urls.py
+++ crud/urls.py
@@ -1,6 +1,6 @@
 """crud URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information please seee:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
